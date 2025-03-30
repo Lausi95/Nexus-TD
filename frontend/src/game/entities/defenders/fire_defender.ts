f7c5a97252f266e1aee79227f4ea0f2254794b39
Tom Lausmann <tom.lausmann@adesso.de>
2025-03-31 01:06:07 +0200

    feat: add elemental turrets

---
+++ frontend/src/game/entities/defenders/fire_defender.ts
@@ -0,0 +1,45 @@
+import { ENTITY_ID } from 'game/enum/entitiy_id.ts';
+import { COLOR } from 'game/enum/colors.ts';
+import Game from 'game/engine/Game.ts';
+import DefenderObject from 'game/engine/DefenderObject.ts';
+import { radiusCross2x } from 'game/enum/effectiveRadius.ts';
+import { ELEMENT_TYPE } from 'game/enum/elementType.ts';
+
+type TProps = {
+  game: Game;
+  placeholderPosition: [number, number];
+  isProjection?: boolean;
+};
+
+export default class FlamethrowerDefener extends DefenderObject {
+  constructor({ game, placeholderPosition, isProjection = false }: TProps) {
+    super({
+      id: ENTITY_ID.BASIC_ENEMY,
+      elementType: ELEMENT_TYPE.FIRE,
+      name: 'Fire Turret',
+      game,
+      placeholderPosition,
+      isProjection,
+      effectiveRadius: radiusCross2x,
+    });
+  }
+
+  draw(context: any) {
+    this.drawTargetTracing(context);
+    if (this.gameObject.isProjection) {
+      this.drawProjection(context);
+    }
+
+    context.fillStyle = COLOR.ORANGE;
+    context.fillRect(
+      this.gameObject.position.x + 10,
+      this.gameObject.position.y + 10,
+      20,
+      20,
+    );
+  }
+
+  update(_deltaTime: number) {
+    this.targetAndDamageEnemies();
+  }
+}

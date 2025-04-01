5c3d0bccfde5f3baa3dbf37a607902f6ca42ff14
Tom Lausmann <tomlausmann@gmail.com>
2025-04-01 02:53:55 +0200

    refactor: Nature Defender

--- frontend/src/game/entities/defenders/nature_defender.ts
+++ frontend/src/game/entities/defenders/nature_defender.ts
@@ -18,12 +18,15 @@ type NatureProjectile = {
   target: AttackerObject;
   position: XY;
   velocity: number;
-  amp: number;
+  acceleration: number;
 };
 
 export default class NatureDefender extends DefenderObject {
   projectiles: NatureProjectile[];
   lastProjectileTime: number;
+  projectilesPerSecond: number;
+  projectileBatchSize: number;
+  projectileSpawnInterval: number;
 
   constructor({ game, placeholderPosition, isProjection = false }: TProps) {
     super({
@@ -37,6 +40,10 @@ export default class NatureDefender extends DefenderObject {
     });
     this.projectiles = [];
     this.lastProjectileTime = 0;
+    this.projectilesPerSecond = 6;
+    this.projectileBatchSize = 1;
+    this.projectileSpawnInterval =
+      (1000 * this.projectileBatchSize) / this.projectilesPerSecond;
   }
 
   draw(context: any) {
@@ -65,29 +72,10 @@ export default class NatureDefender extends DefenderObject {
   update(_deltaTime: number) {
     const dt = _deltaTime / 1000;
 
-    // spawn new projectile
-    if (this.gameObject.game.now - this.lastProjectileTime > 400) {
-      this.gameObject.game.now;
-
-      const attackersInRange = this.gameObject.game.attackerObjects.filter(
-        (attacker) =>
-          isEnemyInAttackRange(
-            this.gameObject.effectiveRadius,
-            [this.gameObject.position.x, this.gameObject.position.y],
-            [attacker.gameObject.position.x, attacker.gameObject.position.y],
-          ),
-      );
-
-      if (attackersInRange.length > 0) {
-        for (let i = 0; i < 3; i++) {
-          const targetIndex = Math.floor(
-            Math.random() * attackersInRange.length,
-          );
-          const target = attackersInRange[targetIndex];
-          this.projectiles.push(this.getProjectile(target));
-        }
-      }
-
+    const timeSinceLastProjectileSpawn =
+      this.gameObject.game.now - this.lastProjectileTime;
+    if (timeSinceLastProjectileSpawn > this.projectileSpawnInterval) {
+      this.spawnProjectiles();
       this.lastProjectileTime = this.gameObject.game.now;
     }
 
@@ -100,23 +88,22 @@ export default class NatureDefender extends DefenderObject {
       }
 
       // move and rotate projectile to target direction
-      const diffVec = subVec2(
+      const vecToTarget = subVec2(
         projectile.target.gameObject.position,
         projectile.position,
       );
 
-      const targetVector = normalizeVec2(diffVec);
+      const direction = normalizeVec2(vecToTarget);
 
-      const newPosition = addVec2(
+      projectile.position = addVec2(
         projectile.position,
-        mulVec2(targetVector, projectile.velocity * dt),
+        mulVec2(direction, projectile.velocity * dt),
       );
 
-      projectile.position = newPosition;
-      projectile.velocity = projectile.velocity + projectile.amp * dt;
+      projectile.velocity = projectile.velocity + projectile.acceleration * dt;
 
       // remove projectile if close to enemy and apply damage
-      if (lengthVec2(diffVec) < 10) {
+      if (lengthVec2(vecToTarget) < 10) {
         this.enemiesTargeted.push(projectile.target);
         deadProjectiles.push(projectile);
       }
@@ -132,37 +119,74 @@ export default class NatureDefender extends DefenderObject {
     this.enemiesTargeted = [];
   }
 
-  getProjectile(target: AttackerObject): NatureProjectile {
+  spawnProjectiles() {
+    const attackersInRange = this.getAttackersInRange();
+    if (attackersInRange.length > 0) {
+      for (let i = 0; i < this.projectileBatchSize; i++) {
+        const target = selectRandomItem(attackersInRange)!;
+        this.spawnProjectile(target);
+      }
+    }
+  }
+
+  spawnProjectile(target: AttackerObject) {
     const offset = mulVec2(randomVec2(), 6);
-    return {
+    const projectile: NatureProjectile = {
       target: target,
       position: {
         x: this.gameObject.position.x + 20 + offset.x,
         y: this.gameObject.position.y + 20 + offset.y,
       },
       velocity: 0,
-      amp: 100,
+      acceleration: 100,
     };
+    this.projectiles.push(projectile);
+  }
+
+  getAttackersInRange(): AttackerObject[] {
+    return this.gameObject.game.attackerObjects.filter((attacker) =>
+      isEnemyInAttackRange(
+        this.gameObject.effectiveRadius,
+        [this.gameObject.position.x, this.gameObject.position.y],
+        [attacker.gameObject.position.x, attacker.gameObject.position.y],
+      ),
+    );
   }
 }
 
+/**
+ * Normalizes a vector.
+ * Normalized means, that the length of the vector is 1.
+ */
 function normalizeVec2(vec: XY): XY {
   const length = lengthVec2(vec);
   return { x: vec.x / length, y: vec.y / length };
 }
 
+/**
+ * Adds two vectors and returns the vector sum.
+ */
 function addVec2(vec1: XY, vec2: XY): XY {
   return { x: vec1.x + vec2.x, y: vec1.y + vec2.y };
 }
 
+/**
+ * Subtracts two vectors and returns the vector difference.
+ */
 function subVec2(vec1: XY, vec2: XY): XY {
   return { x: vec1.x - vec2.x, y: vec1.y - vec2.y };
 }
 
+/**
+ * Determines the length of a vector and returns the length.
+ */
 function lengthVec2(vec: XY): number {
   return Math.sqrt(vec.x * vec.x + vec.y * vec.y);
 }
 
+/**
+ * Multiplies the vector with a factor and returns the resulting vector.
+ */
 function mulVec2(vec: XY, factor: number): XY {
   return {
     x: vec.x * factor,
@@ -171,7 +195,8 @@ function mulVec2(vec: XY, factor: number): XY {
 }
 
 /**
- * Returns a random normalized vector.
+ * Creates a new random normalized vector.
+ * Normalized means, that the length of the vector is 1.
  */
 function randomVec2(): XY {
   const random = Math.random() * 2 * Math.PI;
@@ -179,4 +204,19 @@ function randomVec2(): XY {
     x: Math.sin(random),
     y: Math.cos(random),
   };
+}
+
+/**
+ * Returns a random element of the given array.
+ * If the array is empy, `undefined` is returned.
+ *
+ * @param items Array to select a random item from
+ * @returns T Random item, or `undefined`
+ */
+function selectRandomItem<T>(items: T[]): T | undefined {
+  if (items.length === 0) {
+    return undefined;
+  }
+  const randomIndex = Math.floor(Math.random() * items.length);
+  return items[randomIndex];
 }
